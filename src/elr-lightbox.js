af6adbbe69c0649aae6102476aed3a518f1a2ed6
Elizabeth Rogers <beth3346@gmail.com>
2016-11-29 15:34:50 -0600

    updated to es6 syntax

--- src/elr-lightbox.js
+++ src/elr-lightbox.js
@@ -1,142 +1,157 @@
-(function($) {
-    window.elrLightbox = function(params) {
-        var self = {};
-        var spec = params || {};
-        var $images = spec.images || $('ul.elr-lightbox-thumbnails');
-        var speed = spec.speed || 300;
+import elrUtlities from 'elr-utilities';
+const $ = require('jquery');
 
-        var createThumbnails = function(list) {
-            var $links = list.find('a');
-            var thumbnailList = [];
-            var thumbnails = '';
+let elr = elrUtlities();
+
+const elrLightbox = function({
+    $images = $('ul.elr-lightbox-thumbnails'),
+    speed = 300
+} = {}) {
+    const self = {
+        createThumbnails(list) {
+            const $links = list.find('a');
+            let thumbnailList = [];
+            let thumbnails = '';
 
             $links.each(function() {
                 thumbnailList.push($(this).attr('href'));
             });
 
             $.each(thumbnailList, function(k, v) {
-                thumbnails += '<li><a href=' + v + '><img src=' + v + ' /></a></li>';
+                thumbnails += `<li><a href=${v}><img src=${v}></a></li>`;
             });
 
             return thumbnails;
-        };
+        },
 
-        var createLightbox = function(thumbnails, speed) {
-            var img = $(this).attr('href');
-            
-            var $imgVisible = elr.createElement('img', {
-                'class': 'img-visible',
-                'src': img,
-                'alt': 'thumbnail'
+        createLightbox(thumbnails, speed) {
+            const img = $(this).attr('href');
+
+            const $imgVisible = elr.createElement('img', {
+                class: 'img-visible',
+                src: img,
+                alt: 'thumbnail'
             });
 
-            var $close = elr.createElement('button', {
-                'class': 'close',
+            const $close = elr.createElement('button', {
+                class: 'close',
                 text: 'x'
             });
 
-            var $thumbnails = elr.createElement('ul', {
-                'class': 'thumbnail-list',
+            const $thumbnails = elr.createElement('ul', {
+                class: 'thumbnail-list',
                 html: thumbnails
             });
 
-            var $nav = elr.createElement('div', {
-                'class': 'lightbox-nav',
-                html: '<button class="prev" data-dir="prev"><i class="fa fa-caret-left"></i></button><button class="next" data-dir="next"><i class="fa fa-caret-right"></i></button>'
+            const $nav = elr.createElement('div', {
+                class: 'lightbox-nav',
+                html: `<button class="prev" data-dir="prev"><i class="fa fa-caret-left"></i></button><button class="next" data-dir="next"><i class="fa fa-caret-right"></i></button>`
             });
 
-            var $lightbox = elr.createElement('div', {
-                'class': 'elr-blackout'
+            const $lightbox = elr.createElement('div', {
+                class: 'elr-blackout'
             });
-            
+
             $lightbox.hide().appendTo('body').fadeIn(speed, function() {
                 $close.appendTo($lightbox);
                 $imgVisible.appendTo($lightbox);
                 $nav.appendTo($lightbox);
                 $thumbnails.appendTo($lightbox);
             });
-        };
+        },
 
-        var advanceImage = function(direction) {
-            var $list = $('.thumbnail-list');
-            var $currentImg = $('div.elr-blackout img.img-visible');
-            var currentImgSrc = $currentImg.attr('src');
-            var $currentThumb = $list.find('img[src$="' + currentImgSrc + '"]').closest('li').index();
-            var len = $list.find('li').length - 1;
-            var $nextImg;
-            var nextImgIndex;
+        getNextImageIndex(direction, $list, $currentImg) {
+            const currentImgSrc = $currentImg.attr('src');
+            const $currentThumb = $list.find(`img[src$="${currentImgSrc}"]`).closest('li').index();
+            const len = $list.find('li').length - 1;
 
-            if ( direction === 'prev' ) {
-                nextImgIndex = ($currentThumb === 0) ? len : $currentThumb - 1;
-            } else {
-                nextImgIndex = ($currentThumb === len) ? 0 : $currentThumb + 1;
+            if (direction === 'prev') {
+                return ($currentThumb === 0) ? len : $currentThumb - 1;
             }
 
-            $nextImg = $list.find('li').eq(nextImgIndex).find('img').attr('src');
+            return ($currentThumb === len) ? 0 : $currentThumb + 1;
+        },
+
+        advanceImage(direction, speed) {
+            const $list = $('.thumbnail-list');
+            const $currentImg = $('div.elr-blackout img.img-visible');
+            const $nextImg = $list.find('li').eq(this.getNextImageIndex(direction, $list, $currentImg)).find('img').attr('src');
+
             $currentImg.fadeOut(speed, function() {
                 $(this).attr('src', $nextImg).fadeIn(speed);
             });
-        };
+        },
 
-        var changeImage = function() {
-            var img = $(this).attr('href');
-            var $oldImg = $('div.elr-blackout img.img-visible');
-            var oldImgSrc = $oldImg.attr('src');
+        changeImage(speed) {
+            const img = $(this).attr('href');
+            const $oldImg = $('div.elr-blackout img.img-visible');
+            const oldImgSrc = $oldImg.attr('src');
 
-            if ( oldImgSrc !== img ) {
+            if (oldImgSrc !== img) {
                 $oldImg.fadeOut(speed, function() {
                     $(this).attr('src', img).fadeIn(speed);
                 });
             }
-        };
+        },
 
-        var removeLightbox = function() {
+        removeLightbox(speed) {
             $('div.elr-blackout').fadeOut(speed, function() {
                 $(this).remove();
             });
-        };
-
-        if ( $images.length ) {
-            var thumbnails = createThumbnails($images);
-            var $body = $('body');
-
-            $images.on('click', 'a', function(e) {
-                e.preventDefault();
-                createLightbox.call(this, thumbnails, speed);
-            });
-
-            $body.on('click', 'div.elr-blackout button.close', removeLightbox);
-            $body.on('click', 'div.elr-blackout', function(e) {
-                removeLightbox();
-                e.stopPropagation();
-            });
-
-            $body.on('click', 'div.elr-blackout ul.thumbnail-list a', function(e) {
-                e.preventDefault();
-                e.stopPropagation();
-                changeImage.call(this);
-            });
-
-            elr.killEvent($body, 'click', 'div.elr-blackout .img-visible');
-            elr.killEvent($body, 'click', 'div.elr-blackout .lightbox-nav');
-
-            $body.on('click', 'div.elr-blackout .lightbox-nav button', function(e) {
-                var direction = $(this).data('dir');
-
-                e.preventDefault();
-                e.stopPropagation();
-                advanceImage(direction);
-            });
-
-            $body.on('keydown', function(e) {
-                if (e.which === 37) {   
-                    advanceImage('prev');
-                } else if (e.which === 39) {
-                    advanceImage('next');
-                } 
-            });
         }
-
-        return self;
     };
-})(jQuery);
+
+    if ($images.length) {
+        const thumbnails = self.createThumbnails($images);
+        const $body = $('body');
+
+        $images.on('click', 'a', function(e) {
+            e.preventDefault();
+            self.createLightbox.call(this, thumbnails, speed);
+        });
+
+        $body.on('click', 'div.elr-blackout button.close', self.removeLightbox);
+        $body.on('click', 'div.elr-blackout', function(e) {
+            e.stopPropagation();
+            self.removeLightbox(speed);
+        });
+
+        $body.on('click', 'div.elr-blackout ul.thumbnail-list a', function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            self.changeImage.call(this, speed);
+        });
+
+        elr.killEvent($body, 'click', 'div.elr-blackout .img-visible');
+        elr.killEvent($body, 'click', 'div.elr-blackout .lightbox-nav button');
+
+        $body.on('click', 'div.elr-blackout .lightbox-nav button', function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+
+            const direction = $(this).data('dir');
+
+            self.advanceImage(direction, speed);
+        });
+
+        // page image slideshow when user hits left/right arrow keys
+        $body.on('keydown', function(e) {
+            if (e.which === 37) {
+                self.advanceImage('prev', speed);
+            } else if (e.which === 39) {
+                self.advanceImage('next', speed);
+            }
+        });
+
+        // close lightbox when user hits the escape key
+        $body.on('keydown', function(e) {
+            if (e.which === 27) {
+                self.removeLightbox();
+            }
+        });
+    }
+
+    return self;
+};
+
+export default elrLightbox;
